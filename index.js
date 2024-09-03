33c68366bc625a8fdcda6481d0e74db48c9b16db
S-via <168160925+S-via@users.noreply.github.com>
2024-09-02 21:39:57 -0700

    added pseudo code for shapes.js to initiate classes for each shape

--- index.js
+++ index.js
@@ -23,5 +23,34 @@ inquirer.prompt([
 // Blank SVG LOGO  with if statements for each option of user input 
 // if user chooses circle put into a string    -->for each answers we writefile (:
 
+// const circleOption = 
+/*
+`${<svg version="1.1"
+width="300" height="200"
+xmlns="http://www.w3.org/2000/svg">
+
+<circle cx="150" cy="100" r="80" fill="green"></circle>
+
+<text x="150" y="125" font-size="40" text-anchor="middle" fill="white">SVG</text>
+</svg>}`*/
+
+// const triangleOption =
+/* 
+`${<svg version="1.1" 
+width="300" height="200" 
+xmlns="http://www.w3.org/2000/svg">
+
+    <polygon points="150,10 240,180 60,180" fill="lime"></polygon> 
+    <text x="146" y="170" font-size="40" text-anchor="middle" fill="white">SVG</text>
+    </svg>}` */
+
+// const rectangleOption = 
+/*
+`${ <svg version="1.1" 
+width="300" height="200" 
+ xmlns="http://www.w3.org/2000/svg">
+    <rect x="65" y="40" width="150" height="100" fill="#9EDBEA"></rect>
+    <text x="135" y="97" font-size="40" text-anchor="middle" fill="white">SVG</text>
+    </svg>}` */
 
 
