1febb26db02e99d431df21359a9d346e124b31ed
S-via <168160925+S-via@users.noreply.github.com>
2024-09-05 13:43:09 -0700

    for shapes.js added constructor function

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 
 inquirer.prompt([
     { type: 'input', name: 'text', message: 'Input 3 character text' },
-    { type: 'input', name: 'textcolor', message: 'Enter color for your text', },
+    { type: 'input', name: 'textColor', message: 'Enter color for your text', },
     { type: 'list', name: 'shape', message: 'Choose the shape you would like', choices: ["circle", "triangle", "rectangle",] },
-    { type: 'input', name: 'shapecolor', message: 'Enter the color for your shape' }])
+    { type: 'input', name: 'shapeColor', message: 'Enter the color for your shape' }])
   
 // create error for 3 charter length 
 // `` 
@@ -21,14 +21,14 @@ function returnStrings(answers) {
    THEN append shapesArray w/ return THEN write file } */
     if (answers.shape === "circle")
         return shapesArray += ` 
-    <circle cx="150" cy="100" r="80" fill=" ${answers.shapecolor} "></circle> <text x="150" y="125" font-size="40" text-anchor="middle" fill=" ${answers.textcolor}">SVG</text></svg>
+    <circle cx="150" cy="100" r="80" fill=" ${answers.shapecolor} "></circle> <text x="150" y="125" font-size="40" text-anchor="middle" fill=" ${answers.textcolor}">${answers.text}</text></svg>
     ` ;
 
     /* if (answers.shape === "triangle")
     THEN append shapesArray + return THEN write file } */
     else if (answers.shape === "triangle")
         return shapesArray += `
-        <polygon points="150,10 240,180 60,180" fill="${answers.shapecolor}"></polygon><text x="146" y="170" font-size="40" text-anchor="middle" fill="${answers.textcolor}">SVG</text></svg>}
+        <polygon points="150,10 240,180 60,180" fill="${answers.shapecolor}"></polygon><text x="146" y="170" font-size="40" text-anchor="middle" fill="${answers.textcolor}">${answers.text}</text></svg>}
         `;
 
 
@@ -36,7 +36,7 @@ function returnStrings(answers) {
     THEN append shapesArray + return THEN write file} */
     else (answers.shape === "rectangle")
     return shapesArray += `
-            <rect x="65" y="40" width="150" height="100" fill="${answers.shapecolor}"></rect><text x="135" y="97" font-size="40" text-anchor="middle" fill="${answers.textcolor}">SVG</text></svg>}
+            <rect x="65" y="40" width="150" height="100" fill="${answers.shapecolor}"></rect><text x="135" y="97" font-size="40" text-anchor="middle" fill="${answers.textcolor}">${answers.text}</text></svg>}
             `;
         }
         console.log(shapesArray);          // ??
