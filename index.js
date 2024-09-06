f7cf7b909257f7398e5cc6780fb113fdacb32ef5
S-via <168160925+S-via@users.noreply.github.com>
2024-09-05 22:50:22 -0700

    added the rest of pseduo code to get possible the code to run

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+// const requirer = requirer ('./lib/shapes') ??
 
 
 inquirer.prompt([
@@ -10,45 +11,43 @@ inquirer.prompt([
     .then((answers) => {returnStrings(answers)});
 
 
-
-// create error for 3 charter length 
-// `` 
-// write file inside folder examples/logos.svg 
-
-// Blank SVG LOGO  with if statements for each shape option 
-// if user chooses circle/tirangle/rectangle add shapesArray + return string --> then for each answer we writefile (:
-
-function returnStrings(answers) {
-    let shapesArray = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-
-    /*   if (answers.shape === "circle")
-   THEN append shapesArray w/ return THEN write file } */
-    if (answers.shape === "circle")
-        return shapesArray += ` 
-    <circle cx="150" cy="100" r="80" fill=" ${answers.shapecolor} "></circle> <text x="150" y="125" font-size="40" text-anchor="middle" fill=" ${answers.textcolor}">${answers.text}</text></svg>
-    ` ;
-
-    /* if (answers.shape === "triangle")
-    THEN append shapesArray + return THEN write file } */
-    else if (answers.shape === "triangle")
-        return shapesArray += `
+    // shapesArray carries first part of   with if statements for each shape option 
+    // if user chooses circle/tirangle/rectangle it will add to string to shapesArray --> then for each option we writefile (:
+    
+    function returnStrings(answers) {
+        
+        
+        let shapesArray = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+        
+        /*   if (answers.shape === "circle")
+        THEN add to shapesArray w/ returning full string  */
+        if (answers.shape === "circle")
+            return shapesArray += ` 
+        <circle cx="150" cy="100" r="80" fill=" ${answers.shapecolor} "></circle> <text x="150" y="125" font-size="40" text-anchor="middle" fill=" ${answers.textcolor}">${answers.text}</text></svg>
+        ` ;
+        
+        /* if (answers.shape === "triangle")
+        THEN add to shapesArray w/ returning full string */
+        else if (answers.shape === "triangle")
+            return shapesArray += `
         <polygon points="150,10 240,180 60,180" fill="${answers.shapecolor}"></polygon><text x="146" y="170" font-size="40" text-anchor="middle" fill="${answers.textcolor}">${answers.text}</text></svg>
         `;
+        
+        
+        /* if (answers.shape === "rectangle")
+        THEN add to shapesArray w/ returning full string */
+        else (answers.shape === "rectangle")
+        return shapesArray += `
+        <rect x="65" y="40" width="150" height="100" fill="${answers.shapecolor}"></rect><text x="135" y="97" font-size="40" text-anchor="middle" fill="${answers.textcolor}">${answers.text}</text></svg>
+        `;
+    }
+    // store function inside fileData variable 
+    const fileData = returnStrings(answers)
+    // write file inside folder examples/logos.svg // and make variable a string 
 
-
-    /* if (answers.shape === "rectangle")
-    THEN append shapesArray + return THEN write file} */
-    else (answers.shape === "rectangle")
-    return shapesArray += `
-            <rect x="65" y="40" width="150" height="100" fill="${answers.shapecolor}"></rect><text x="135" y="97" font-size="40" text-anchor="middle" fill="${answers.textcolor}">${answers.text}</text></svg>
-            `;
-}
-
-/* fs.writeFile('logos.svg'+'./examples', data)
-if (err) { console.log(err); }
-console.log("Generated Logo.svg is printed in the command line"); */
-
-/*   writeToFile('logos.svg',returnStrings(answers));
-*/
-
+    fs.writeFile('/examples/logo.svg', JSON.stringify(fileData), (err) => {
+        if (err) { 
+            console.error(err); }
+   else {
+console.log("Generated Logo.svg is printed in the command line");  }}); 
 
